8785eeb37f1c0183e9d64d5238ac4c42f757cf5e
twopats <lpatipat@icloud.com>
2022-12-05 13:20:01 -0500

    add scripts for deployment

---
+++ script/NFT.s.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.7;
+
+import "forge-std/Script.sol";
+import "../src/NFT.sol";
+
+contract MyScript is Script {
+    function run() external {
+        //load pk from .env
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        
+        //utility: records calls and contract creations, logged by forge /broadcast
+        vm.startBroadcast(deployerPrivateKey);
+
+        NFT nft = new NFT("NFT_demo", "TUT", "baseUri");
+
+        vm.stopBroadcast();
+    }
+}
